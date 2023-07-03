cc5579e4a02cee1adf8557e9a53cc143c01c512d
Ryan Foster-Hill <rfoster-hill@hotmail.com>
2023-07-03 15:07:11 +0100

    first commit

---
+++ dotNotation.js
@@ -0,0 +1,12 @@
+let walls = ["  |     |", "-----------"];
+
+// for (let i = 0; i < 3; i++) {
+//     console.log(walls[0]);
+//     console.log(walls[0]);
+//     console.log(walls[0]);
+//     if (i < 2) {
+//         console.log(walls[1]);
+//     }
+    
+console.log("All around the word".charAt(8).toUpperCase());
+
